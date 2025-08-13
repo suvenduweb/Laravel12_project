27a8b25ba3c43c0ae32e47f0447215125d316d0a
suvendu-bhowmik <suvenduweb@gmail.com>
2025-08-13 22:55:38 +0530

    new changes

--- resources/views/home/homelayout/answers.blade.php
+++ resources/views/home/homelayout/answers.blade.php
@@ -8,66 +8,26 @@
       </div>
       <div class="lonyo-faq-shape"></div>
       <div class="lonyo-faq-wrap1">
-        <div class="lonyo-faq-item item2 open" data-aos="fade-up" data-aos-duration="500">
+        @php
+           $faq = App\Models\Faq::latest()->limit(5)->get();
+        @endphp
+        @foreach ($faq  as $item)
+
+
+        <div class="lonyo-faq-item item2 " data-aos="fade-up" data-aos-duration="500">
           <div class="lonyo-faq-header">
-            <h4>Is my financial data safe and secure?</h4>
+            <h4>{{$item->title}}</h4>
             <div class="lonyo-active-icon">
               <img class="plasicon" src="{{asset('frontend/assets/images/v1/mynus.svg')}}" alt="">
               <img class="mynusicon" src="{{asset('frontend/assets/images/v1/plas.svg')}}" alt="">
             </div>
           </div>
           <div class="lonyo-faq-body body2">
-            <p>Yes, this finance apps use bank-level encryption, multi-factor authentication, and other security measures to protect your sensitive information.</p>
+            <p>{{$item->description}}</p>
           </div>
         </div>
-        <div class="lonyo-faq-item item2" data-aos="fade-up" data-aos-duration="700">
-          <div class="lonyo-faq-header">
-            <h4>Can I link multiple bank accounts and credit cards?</h4>
-            <div class="lonyo-active-icon">
-              <img class="plasicon" src="{{asset('frontend/assets/images/v1/mynus.svg')}}" alt="">
-              <img class="mynusicon" src="{{asset('frontend/assets/images/v1/plas.svg')}}" alt="">
-            </div>
-          </div>
-          <div class="lonyo-faq-body body2">
-            <p>Yes, this finance apps use bank-level encryption, multi-factor authentication, and other security measures to protect your sensitive information.</p>
-          </div>
-        </div>
-        <div class="lonyo-faq-item item2" data-aos="fade-up" data-aos-duration="900">
-          <div class="lonyo-faq-header">
-            <h4>How does the app help me stick to my budget?</h4>
-            <div class="lonyo-active-icon">
-              <img class="plasicon" src="{{asset('frontend/assets/images/v1/mynus.svg')}}" alt="">
-              <img class="mynusicon" src="{{asset('frontend/assets/images/v1/plas.svg')}}" alt="">
-            </div>
-          </div>
-          <div class="lonyo-faq-body body2">
-            <p>Yes, this finance apps use bank-level encryption, multi-factor authentication, and other security measures to protect your sensitive information.</p>
-          </div>
-        </div>
-        <div class="lonyo-faq-item item2" data-aos="fade-up" data-aos-duration="1100">
-          <div class="lonyo-faq-header">
-            <h4>Can I track my investments with the app?</h4>
-            <div class="lonyo-active-icon">
-              <img class="plasicon" src="{{asset('frontend/assets/images/v1/mynus.svg')}}" alt="">
-              <img class="mynusicon" src="{{asset('frontend/assets/images/v1/plas.svg')}}" alt="">
-            </div>
-          </div>
-          <div class="lonyo-faq-body body2">
-            <p>Yes, this finance apps use bank-level encryption, multi-factor authentication, and other security measures to protect your sensitive information.</p>
-          </div>
-        </div>
-        <div class="lonyo-faq-item item2" data-aos="fade-up" data-aos-duration="1300">
-          <div class="lonyo-faq-header">
-            <h4>Is the app free, or are there subscription fees?</h4>
-            <div class="lonyo-active-icon">
-              <img class="plasicon" src="{{asset('frontend/assets/images/v1/mynus.svg')}}" alt="">
-              <img class="mynusicon" src="{{asset('frontend/assets/images/v1/plas.svg')}}" alt="">
-            </div>
-          </div>
-          <div class="lonyo-faq-body body2">
-            <p>Yes, this finance apps use bank-level encryption, multi-factor authentication, and other security measures to protect your sensitive information.</p>
-          </div>
-        </div>
+        @endforeach
+
       </div>
       <div class="faq-btn" data-aos="fade-up" data-aos-duration="700">
         <a class="lonyo-default-btn faq-btn2" href="faq.html">Can't find your answer</a>
